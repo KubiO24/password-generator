5b3d10eda49d7d96b2f97f925b37f719d5cb1e5f
KubiO24 <kubakowal2004@gmail.com>
2023-02-22 16:43:13 +0100

    Add scss modules and character length state

--- src/modules/HomeModule/pages/HomePage/HomePage.tsx
+++ src/modules/HomeModule/pages/HomePage/HomePage.tsx
@@ -1,24 +1,22 @@
 import React, { useState } from "react";
-import "./HomePage.scss";
+import styles from "./HomePage.module.scss";
 
 export const HomePage: React.FC = () => {
-  const [count, setCount] = useState(0);
+    const [characterLength, setCharacterLength] = useState<number>(6);
 
-  return (
-    <>
-      <h1>Helo</h1>
-      <div className="card">
-        <p>
-          See the README.md for more information on how to start your challenge.
-        </p>
-        <button
-          onClick={() => {
-            setCount((count) => count + 1);
-          }}
-        >
-          count is {count}
-        </button>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <h1>Helo</h1>
+            <div className={styles.card}>
+                <p>See the README.md for more information on how to start your challenge.</p>
+                <button
+                    onClick={() => {
+                        setCharacterLength((characterLength) => characterLength + 1);
+                    }}
+                >
+                    count is {characterLength}
+                </button>
+            </div>
+        </>
+    );
 };
